1abc07b23b8d8e1d12b8a84dc5104bf7e0b7f310
maltesilber <malte.silbernagel@web.de>
2025-08-13 13:14:58 +0200

    inital commit

--- DinoV2Code/train.py
+++ DinoV2Code/train.py
@@ -19,7 +19,7 @@ class Config:
         self.learning_rate = 3e-4
         self.weight_decay = 1e-6
         self.image_size = 518  # Default image size for ViT models
-        self.num_workers = 24
+        self.num_workers = 14
 
 def get_dataloaders(config):
     # Load metadata
